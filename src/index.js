bc21f9776e00890cb68bdb9d7a0a46614845e722
Ambrance <ambrance@mail.ru>
2019-10-03 13:05:51 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+  let firstArr = first
+    .split('')
+    .reverse()
+    .map(el => Number(el));
+  let secondArr = second
+    .split('')
+    .reverse()
+    .map(el => Number(el));
+  let res = [];
+  for (let k = 0; k < firstArr.length; k++) {
+    for (let i = 0; i < secondArr.length; i++) {
+      let sum = firstArr[k] * secondArr[i];
+      if (res[k + i]) {
+        res[k + i] += sum;
+      } else {
+        res[k + i] = sum;
+      }
+    }
+  }
+  for (let j = 0; j < res.length; j++) {
+    let p = res[j] % 10;
+    let q = Math.floor(res[j] / 10);
+    res[j] = p;
+    if (res[j + 1]) {
+      res[j + 1] += q;
+    } else if (q) {
+      res[j + 1] = q;
+    }
+  }
+  return res.reverse().join('');
+  /*let res = BigInt(first) * BigInt(second);
+  return res.toString();*/
+};
